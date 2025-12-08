c50aa835b20e9dc517a79f906fe184bc6305681e
Nayum Shaikh <nayumshaikh189@gmail.com>
2025-12-08 22:32:54 +0530

    new file added. Method calling. Commit and Push

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    public static void msg(String msg)
+    {
+        System.out.println(msg);
+
+    }
+    public static void main(String[] args)
+    {
+        msg("Hello World");
+        }
+}
+
