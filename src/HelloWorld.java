1c2d5ccf409e020c28d0b488302a2635317c1060
Nayum Shaikh <nayumshaikh189@gmail.com>
2025-12-12 09:13:00 +0530

    Swapping of numbers using two variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.List.*;
+
 public class HelloWorld {
 
     public static void msg(String msg)
@@ -8,6 +14,11 @@ public class HelloWorld {
     public static void main(String[] args)
     {
         msg("Hello World");
+        int[] a ={1,3,4,5,6};
+        List<Integer> x = Arrays.stream(a).boxed().collect(Collectors.toList());
+
+        List<Integer> i = Arrays.asList(1,2,3,4,5);
+        System.out.println(i);
         }
 }
 
---
+++ src/Practice/P01_Swap_numbers.java
@@ -0,0 +1,28 @@
+package Practice;
+
+public class P01_Swap_numbers
+{
+
+
+    public static void swapNumber(int a, int b)
+    {
+        int ax = a;
+        int by=b;
+
+        ax=ax+by;
+        by = ax-by;
+        ax=ax-by;
+
+        System.out.println("After swap"+ax+","+by);
+
+
+    }
+
+    public static void main(String args[])
+    {
+        int a=5;
+        int b=4;
+        System.out.println("Before swap"+a+","+b);
+    swapNumber(a,b);
+    }
+}
