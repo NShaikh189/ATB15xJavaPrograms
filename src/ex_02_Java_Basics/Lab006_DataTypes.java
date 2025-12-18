55a277b20dd81f8965ec380da5e027214d9d89a5
Nayum Shaikh <nayumshaikh189@gmail.com>
2025-12-18 09:36:59 +0530

    data types, static variables, static methods

--- src/ex_02_Java_Basics/Lab006_DataTypes.java
+++ src/ex_02_Java_Basics/Lab006_DataTypes.java
@@ -1,13 +1,23 @@
 package ex_02_Java_Basics;
 
-import java.util.Collections;
 
 public class Lab006_DataTypes
 {
-    public static void main(String[] args) {
+     static void main(String[] args) {
+        byte b =127; // -128 to 127 = 2^7
+        short sh = 32767; // -32768 to 32767 = 2^15
+        int in = 343432239; //-2,147,483,648 to 2,147,483,647
+        long l = 9762341888L; //2^63
+        float f = 3.14566f; //upto 7 decimal digits
+        boolean flag = true;
         char ch = 250;
         System.out.println(Character.valueOf(ch));
         System.out.printf("%c",ch);
-
+        System.out.println(sh);
+        System.out.println(b);
+        System.out.println(in);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(flag);
     }
 }
