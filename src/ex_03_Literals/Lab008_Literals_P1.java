53d3f5af146abd4d08674dea9e713c09d159735a
Nayum Shaikh <nayumshaikh189@gmail.com>
2025-12-19 08:07:47 +0530

    Literals

---
+++ src/ex_03_Literals/Lab008_Literals_P1.java
@@ -0,0 +1,14 @@
+package ex_03_Literals;
+
+public class Lab008_Literals_P1 {
+    public static void main(String[] args) {
+        int a = 10;
+        //datatype variable operator literal
+        char rupee = '\u20B9';
+        System.out.println(rupee);
+        char rs = '₹';
+        char v=65;
+        System.out.println(rs);
+        System.out.println(v);
+    }
+}
