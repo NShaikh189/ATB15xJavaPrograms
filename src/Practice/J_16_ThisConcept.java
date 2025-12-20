734ce7d41fd63697c931b9cceb071d98a6a6a043
Nayum Shaikh <nayumshaikh189@gmail.com>
2025-12-20 07:06:19 +0530

    final

---
+++ src/Practice/J_16_ThisConcept.java
@@ -0,0 +1,41 @@
+package Practice;
+
+/*
+this() is used to call the current class constructor
+can be used only inside the constructor
+used for constructor chaining - this(), this(10) etc
+It should be the first statement in the constructor
+
+this keyword
+scope is limited to the current class
+refers to the current class object
+access instance variables
+ */
+
+public class J_16_ThisConcept {
+
+    String name;
+    static byte wheels=4;
+    String model;
+    J_16_ThisConcept()
+    {
+        this.name = "Maruti";
+        this.model = "Swift";
+     //   this("Hyundai","Creta");
+    }
+
+    J_16_ThisConcept(String name, String model)
+    {
+        this.name = name;
+        this.model = model;
+            //this();
+    }
+
+    public static void main(String[] args) {
+        J_16_ThisConcept obj1 = new J_16_ThisConcept();
+        System.out.println(obj1.name+" : "+obj1.model+" : "+wheels);
+
+        J_16_ThisConcept obj2 = new J_16_ThisConcept("Hyndai","Creata");
+        System.out.println(obj2.name+" : "+obj2.model+" : "+wheels);
+    }
+}
