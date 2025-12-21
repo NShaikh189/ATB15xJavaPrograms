34b3ab450467292444afa83a0a497ac0d5ad481b
Nayum Shaikh <nayumshaikh189@gmail.com>
2025-12-21 19:43:18 +0530

    Private constructor

--- src/Practice/J_18_PrivateConstructorTest.java
+++ src/Practice/J_18_PrivateConstructorTest.java
@@ -3,7 +3,7 @@ package Practice;
 public class J_18_PrivateConstructorTest {
 
     public static void main() {
-       J_18_PrivateConstructor obj1 = new J_18_PrivateConstructor();
+      // J_18_PrivateConstructor obj1 = new J_18_PrivateConstructor();
         //ERROR: J_18_PrivateConstructor()' has private access in 'Practice.J_18_PrivateConstructor'
 
     }
