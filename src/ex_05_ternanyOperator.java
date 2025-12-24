4ceffab4c7455bcb80b7c7ced6ff31f4efaa6ec0
Nayum Shaikh <nayumshaikh189@gmail.com>
2025-12-24 07:58:12 +0530

    Ternanary operators

---
+++ src/ex_05_ternanyOperator.java
@@ -0,0 +1,7 @@
+public class ex_05_ternanyOperator {
+    public static void main() {
+        int marks = 100;
+        String result = marks>=40 ? "Pass" : "Fail";
+        System.out.println(result);
+    }
+}
