90351763614b1633f99f668ed950ec10b38d30fc
Nayum Shaikh <nayumshaikh189@gmail.com>
2025-12-31 08:44:42 +0530

    Code updated

--- src/Practice/Abstraction/LoginPageTest.java
+++ src/Practice/Abstraction/LoginPageTest.java
@@ -2,6 +2,17 @@ package Practice.Abstraction;
 
 public class LoginPageTest {
      static void main() {
+          int a=10, b=6; int c = a+b*5;
+          System.out.println(c);
+
+          float x = 22.54F;
+          float y = 23.5F;
+
+          if(true == true)
+              System.out.println("True");
+          else
+              System.out.println("False");
+
         LoginPage loginPage = new LoginPage();
         loginPage.title();
         loginPage.url();
