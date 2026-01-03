0592e51683d1c19d6aaf68faa65e15b89ead9259
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-01-03 17:47:22 +0530

    String convert 1st character of every word to UpperCase using Streams

---
+++ src/Practice/StringManipulation/P17_StringUpperCaseStreams.java
@@ -0,0 +1,15 @@
+package Practice.StringManipulation;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class P17_StringUpperCaseStreams {
+    public static void main() {
+        String str = "india is my country";
+
+       String newStr = Arrays.stream(str.split(" "))
+               .map(s->s.substring(0,1).toUpperCase().concat(s.substring(1))+" ")
+               .collect(Collectors.joining());
+        System.out.println(newStr);
+    }
+}
