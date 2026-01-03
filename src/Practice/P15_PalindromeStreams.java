628fe2ce2f2f240b1e3fec099599628f0ede6c27
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-01-03 17:11:10 +0530

    Palindrome using streams

--- src/Practice/P15_PalindromeStreams.java
+++ src/Practice/P15_PalindromeStreams.java
@@ -11,8 +11,8 @@ public class P15_PalindromeStreams {
         try {
             System.out.println("Enter the string to check if it is a Palindrome");
             String str = scanner.nextLine();
-            String str1 = str.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
-            boolean isPalindrome = IntStream.range(0, str1.length() / 2).allMatch(i -> str1.charAt(i) == str1.charAt(str1.length() - 1 - i));
+            String str1 = str.replaceAll("[^A-Za-z0-9]","").toLowerCase();
+            boolean isPalindrome = IntStream.range(0, str1.length()/2).allMatch(i->str1.charAt(i)==str1.charAt(str1.length()-1-i));
 
             if (isPalindrome)
                 System.out.println("Is Palindrome");
