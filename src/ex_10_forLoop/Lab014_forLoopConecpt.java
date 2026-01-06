4885e8c91eacda3949225757312cde2ddbb2f249
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-01-06 23:27:04 +0530

    For loop

---
+++ src/ex_10_forLoop/Lab014_forLoopConecpt.java
@@ -0,0 +1,20 @@
+package ex_10_forLoop;
+
+public class Lab014_forLoopConecpt {
+
+    public static void main() {
+        for(int i=0;i<10;i++);
+        {
+            System.out.println("Hello");
+        }
+        System.out.println("World");
+        int x=10;
+        if(x!=10);
+        {
+            System.out.println("Inside if");
+        }
+        System.out.println("outside if");
+
+
+    }
+}
