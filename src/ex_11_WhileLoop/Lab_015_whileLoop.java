0ea47443e7fd10fc891c0e8b56b373048190b7fd
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-01-07 23:39:35 +0530

    while concept

---
+++ src/ex_11_WhileLoop/Lab_015_whileLoop.java
@@ -0,0 +1,13 @@
+package ex_11_WhileLoop;
+
+public class Lab_015_whileLoop {
+
+    public static void main() {
+        int i=0;
+
+        while(i<10)
+        {
+            System.out.println(i++);
+        }
+    }
+}
