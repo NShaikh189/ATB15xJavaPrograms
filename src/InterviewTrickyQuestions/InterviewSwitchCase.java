c7cb2bdcef76a589289d5822450d093c4350d447
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-01-07 23:09:36 +0530

    interview questions

---
+++ src/InterviewTrickyQuestions/InterviewSwitchCase.java
@@ -0,0 +1,14 @@
+package InterviewTrickyQuestions;
+
+public class InterviewSwitchCase {
+    public static void main() {
+        String s = "Active";
+
+        switch(s)
+        {
+            case "Active" :
+                System.out.println("Hello");
+
+        }
+    }
+}
