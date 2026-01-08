6f783a4ec2674e554242c7f3541ce94a68bf6853
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-01-08 23:58:05 +0530

    Swap two nums with third variable

---
+++ src/Practice/P27_SwapTwoNumbesWithThirdVar.java
@@ -0,0 +1,25 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class P27_SwapTwoNumbesWithThirdVar {
+    public static void main() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first number");
+        int num1 = scanner.nextInt();//5
+
+        System.out.println("Enter second number");
+        int num2 = scanner.nextInt();//3
+
+        int temp=0;
+        temp = num1+num2;//8 , 3
+        num1 = temp-num1; // 8 , 5
+        num2 = temp-num2; //3, 5
+
+        System.out.println("Number 1: "+num1);
+        System.out.println("Number 2: "+num2);
+
+        scanner.close();
+    }
+}
