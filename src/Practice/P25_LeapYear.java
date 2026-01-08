b6670ab5c08bf2a5f825dbdc4e43a79de646f4f5
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-01-08 23:49:00 +0530

    Leap Year

---
+++ src/Practice/P25_LeapYear.java
@@ -0,0 +1,14 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class P25_LeapYear {
+    public static void main() {
+        System.out.println("Enter the year");
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+        boolean result = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+        System.out.println(result);
+        scanner.close();
+    }
+}
