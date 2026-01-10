b57fbb0a9428f8e8bc3864849b84378be78848e0
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-01-10 20:35:28 +0530

    Anagram

--- src/Practice/P31_Anagram.java
+++ src/Practice/P31_Anagram.java
@@ -8,9 +8,8 @@ O/p: a14b5c3
  */
 public class P31_Anagram {
     public static void main() {
-        String str = "a12b2c4b3a2";
-        int cnt=0;
-        //int val=0;
+        String str = "av12b2c4b3a2";
+
         HashMap<Character,Integer> hashmap = new HashMap<>();
         for(int i=0;i<str.length();)
         {
@@ -28,15 +27,9 @@ public class P31_Anagram {
                 val.append(ch1);
                 i++;
             }
-            if(!hashmap.containsKey(ch))
-            {
-                hashmap.put(ch,Integer.parseInt(String.valueOf(val)));
-            }
-            else
-            {
-                hashmap.put(ch,hashmap.getOrDefault(ch,0)+Integer.parseInt(String.valueOf(val)));
-            }
-            System.out.println(hashmap.toString());
+
+            int value = (val.length()>0)? (Integer.parseInt(String.valueOf(val))) : 0;
+            hashmap.put(ch,hashmap.getOrDefault(ch,0)+value);
 
         }
         StringBuilder result = new StringBuilder();
