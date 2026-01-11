9870f7d44914d8e5467a1908cd02637691a2d6b5
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-01-11 23:22:37 +0530

    Arithmetic Operations on Two Numbers

--- src/Exam_11_Jan_Programs/P1_ArithmeticOperationsOnTwoNumbers.java
+++ src/Exam_11_Jan_Programs/P1_ArithmeticOperationsOnTwoNumbers.java
@@ -43,26 +43,44 @@ public class P1_ArithmeticOperationsOnTwoNumbers {
 
     static int divide(int a, int b)
     {
-
         return (a/b);
+    }
+
+    static int remainder(int a, int b)
+    {
+        return (a%b);
     }
 
 
     public static void main(String[] args) {
-        System.out.println("Enter num1");
         int result;
         scanner = new Scanner(System.in);
+
+        System.out.println("Enter num1");
         num1 = getInt();
 
         System.out.println("Enter num2");
         num2 = getInt();
 
+        if(num1<=0 || num2<=0)
+        {
+            System.out.println("Entered numbers should be greater than 0");
+            System.exit(0);
+        }
         result = add(num1,num2);
         System.out.println("Addition of "+num1+" and "+num2+" => "+result);
 
         result = substract(num1,num2);
         System.out.println("Substraction of "+num1+" and "+num2+" => "+result);
 
+        result = multiply(num1,num2);
+        System.out.println("Multiplication of "+num1+" and "+num2+" => "+result);
+
+        result = divide(num1,num2);
+        System.out.println("Division of "+num1+" and "+num2+" => "+result);
+
+        result = remainder(num1,num2);
+        System.out.println("Remainder of "+num1+" and "+num2+" => "+result);
 
         scanner.close();
     }
