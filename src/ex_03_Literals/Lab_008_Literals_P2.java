b8f27776143a2ab59193a5c377eb24ebb2646f96
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-01-11 23:13:06 +0530

    Character literal

---
+++ src/ex_03_Literals/Lab_008_Literals_P2.java
@@ -0,0 +1,13 @@
+package ex_03_Literals;
+
+public class Lab_008_Literals_P2 {
+    public static void main(String[] args) {
+        char ch= 'A';
+        char ch1='1';
+
+        System.out.println((int)ch);
+        System.out.println((int)ch1);
+        System.out.println(ch-ch1);
+      //  Integer.valueOf(ch);
+    }
+}
