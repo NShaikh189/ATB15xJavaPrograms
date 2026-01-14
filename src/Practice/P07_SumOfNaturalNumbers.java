5c1bbaf6c8f1afe23433db155c649f0e5565b648
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-01-14 22:38:57 +0530

    Sum of natural numbers using DSA - recursion

--- src/Practice/P07_SumOfNaturalNumbers.java
+++ src/Practice/P07_SumOfNaturalNumbers.java
@@ -1,16 +1,34 @@
 package Practice;
 
+import java.util.Scanner;
+
+//1+2+3 => 3+sum(2) => 3+2+sum(1)
 public class P07_SumOfNaturalNumbers {
 
-    static int sum(int n)
+    static int sumRecursion(int n)
     {
-        return n*(n+1)/2;
+        if(n==1)
+            return 1;
+
+        return n+sumRecursion(n-1);
+    }
+
+    static int sumFormula(int n)
+    {
+        return (n*(n+1))/2;
     }
     public static void main() {
         //Sum of 'n' natural numbers formula: sum = n(n+1)/2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number to calculate the sum of natural numbers");
 
-        int n=15;
-        int sum = sum(n);
-        System.out.println(sum);
+        int n= scanner.nextInt();
+        if(n>0)
+        {
+        System.out.println("Recursion method : "+sumRecursion(n));
+        System.out.println("Sum using Formula: "+sumFormula(n));
+        }
+        else
+            System.out.println("Enter number greater than 0.");
     }
 }
