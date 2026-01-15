498f287780068eae7a10033c6d0b5dca295b4e8d
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-01-15 07:05:11 +0530

    Print numbers 100 to 1

--- src/Practice/P36_Print100numbersUsingRecursion.java
+++ src/Practice/P36_Print100numbersUsingRecursion.java
@@ -2,17 +2,27 @@ package Practice;
 //1,2,3,4......100
 public class P36_Print100numbersUsingRecursion {
 
-    static void printNumbers(int n)
+    static void printNumbers1to100(int n)
     {
         if(n>100)
             return;
 
         System.out.println(n);
-        printNumbers(n+1);
+        printNumbers1to100(n+1);
 
+    }
+
+    static void printNumbers100to1(int n)
+    {
+        if(n<1)
+            return;
+        System.out.println(n);
+        printNumbers100to1(n-1);
     }
     public static void main(String[] args) {
 
-       printNumbers(1);
+       printNumbers1to100(1);
+        System.out.println("=".repeat(10));
+       printNumbers100to1(100);
     }
 }
