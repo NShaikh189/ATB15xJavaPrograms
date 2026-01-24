ae81b42571dba347b2a262e6e120a03436d1d2b3
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-01-24 11:21:51 +0530

    Number Pattern using recursion

--- src/Practice/P40_NumberPattern.java
+++ src/Practice/P40_NumberPattern.java
@@ -10,5 +10,22 @@ public class P40_NumberPattern {
             System.out.print(",");
 
         }
+
+        System.out.println("----------------");
+        numberPattern(10);
     }
+
+    //1,12,123
+    static void numberPattern(int n)
+    {
+        if(n==1)
+            return;
+
+        numberPattern(n-1);
+
+        for(int i=1;i<n;i++)
+        System.out.print(i);
+        System.out.print(",");
+
+        }
 }
