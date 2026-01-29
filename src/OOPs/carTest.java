64b0fed7cd289edf35bc55113c7170616fdf23b6
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-01-29 23:33:59 +0530

    Object

---
+++ src/OOPs/carTest.java
@@ -0,0 +1,9 @@
+package OOPs;
+
+public class carTest {
+
+    public static void main(String args[]) {
+        Car tata = new Car("Nexon",22314,"AMT+","Automatic");
+        tata.getInfo();
+    }
+}
