bfe4e23176f7f17ed45b5774ea806bb93fd51571
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-02-10 19:23:45 +0530

    Person Class

---
+++ src/Exam_10_Feb_Programs/Person.java
@@ -0,0 +1,25 @@
+package Exam_10_Feb_Programs;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void display()
+    {
+        System.out.println("Name: "+name+", Age: "+age);
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person("John",25);
+        Person p2 = new Person("Alice", 30);
+
+        p1.display();
+        p2.display();
+    }
+}
