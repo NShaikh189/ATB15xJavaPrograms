ddd52843130198bbe0b1421910943344ba487068
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-02-10 22:34:07 +0530

    Animal Sound Polymorphism Create a class "Animal" and subclasses "Dog" and "Cat". Override a method to make each animal make a sound.

--- src/Exam_10_Feb_Programs/Polymorphism/AnimalTest.java
+++ src/Exam_10_Feb_Programs/Polymorphism/AnimalTest.java
@@ -3,8 +3,8 @@ package Exam_10_Feb_Programs.Polymorphism;
 public class AnimalTest {
 
     public static void main(String[] args) {
-        Animal dog = new Dog();
-        Animal cat = new Cat();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
 
         dog.makeSound();
         cat.makeSound();
