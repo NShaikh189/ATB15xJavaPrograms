f0ca97d1e046ee33bc9e9bf57e5ba9766d7006a6
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-02-25 23:37:23 +0530

    SingletonConcept

--- src/Singleton/SingletonConcept.java
+++ src/Singleton/SingletonConcept.java
@@ -1,17 +1,22 @@
 package Singleton;
 
 public class SingletonConcept {
-    private static SingletonConcept obj;
+//only one instance should be created
+//constructor is private - Not allowing to create a object outside class
+//private reference variable and static so that only one instance can be used everywhere
 
-    private SingletonConcept(){}
+    private static SingletonConcept singletonConceptObj;
 
-    public static SingletonConcept getObj()
-    {
-        return new SingletonConcept();
+    private SingletonConcept()
+    {}
+
+    public static SingletonConcept getSingletonConceptObj() {
+        return singletonConceptObj = new SingletonConcept();
     }
 
-    public void display()
+
+    public void displayInfo()
     {
-        System.out.println("Display called");
+        System.out.println("Singleton class display method");
     }
 }
