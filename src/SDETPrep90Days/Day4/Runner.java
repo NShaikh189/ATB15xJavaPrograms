8f38b7ae68f1a4e18b706ee0829c8873a6649032
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-02-26 23:57:40 +0530

    Abstract concept

--- src/SDETPrep90Days/Day4/Runner.java
+++ src/SDETPrep90Days/Day4/Runner.java
@@ -1,4 +1,15 @@
 package SDETPrep90Days.Day4;
 
 public class Runner {
+
+
+    void main() {
+
+        Bank hdfcbank = new Bank();
+        hdfcbank.createAccount("Electricity Bill");
+
+
+        Bank hdfcbank2= new Bank();
+        hdfcbank2.createAccount("Property tax Bill");
+    }
 }
