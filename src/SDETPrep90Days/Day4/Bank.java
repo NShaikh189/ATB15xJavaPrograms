dcdbb513314b232e88c6d3ee7c4970cae5804765
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-02-26 23:54:56 +0530

    Abstract concept

---
+++ src/SDETPrep90Days/Day4/Bank.java
@@ -0,0 +1,12 @@
+package SDETPrep90Days.Day4;
+
+public class Bank   {
+
+    public static final String BANK_NAME = "HDFC Bank";
+
+    public void createAccount(String document)
+    {
+        System.out.println("creating account with the document ..."+document);
+        System.out.println("Bank account created successfully");
+    }
+}
