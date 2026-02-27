fde5de34811d95680f6204706c7341d87b9756a5
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-02-27 23:29:47 +0530

    Two sum array

---
+++ src/Practice/P71_TwoSumArray.java
@@ -0,0 +1,14 @@
+package Practice;
+
+import static Practice.P50_TwoSumHashSet.twoSum;
+
+public class P71_TwoSumArray {
+
+    void main()
+    {
+        int nums[] = {2,3,5,6,8,9,10,11};
+        int target = 8;
+
+        twoSum(nums,target);
+    }
+}
