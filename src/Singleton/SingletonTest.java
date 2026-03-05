203b13bd6464d4fc94fb4c6de567eceaa414bd2c
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-03-06 00:00:05 +0530

    Singleton practice Test

--- src/Singleton/SingletonTest.java
+++ src/Singleton/SingletonTest.java
@@ -5,7 +5,16 @@ public class SingletonTest {
     public static void main(String[] args) {
 
         SingletonConcept singletonConceptObj = SingletonConcept.getSingletonConceptObj();
-
         singletonConceptObj.displayInfo();
+
+        //SingletonConcept singletonConceptObj2 = new SingletonConcept();
+
+
+        SingleConcept sc = SingleConcept.getObject();
+        sc.displayInfo();
+
+        SingleConcept sc1 = SingleConcept.getObject();
+        sc1.displayInfo();
+
     }
 }
