dca7787cdce68b7f0f2c84d1f579323d85f7e666
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-03-06 00:18:57 +0530

    reverse string using streams

--- src/Practice/P43_ReverseString.java
+++ src/Practice/P43_ReverseString.java
@@ -1,13 +1,15 @@
 package Practice;
 
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class P43_ReverseString {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
        // String temp="";
-
+            getReverse(str);
         char[] ch = str.toCharArray();
 
         if(str.length()>1)
@@ -20,5 +22,15 @@ public class P43_ReverseString {
         System.out.println("Reverse String : "+str);
 
 
+    }
+
+    public static void getReverse(String str)
+    {
+        String reverseString = IntStream.range(0, str.length()-1)
+                .mapToObj(i->str.charAt(str.length()-1-i))
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+
+        System.out.println(reverseString);
     }
 }
