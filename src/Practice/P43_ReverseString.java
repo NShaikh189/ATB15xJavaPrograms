0fabbbe9a625f5c1b13c153ffd9907e66710268d
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-03-06 06:46:02 +0530

    reverse string using string builder

--- src/Practice/P43_ReverseString.java
+++ src/Practice/P43_ReverseString.java
@@ -10,7 +10,7 @@ public class P43_ReverseString {
         String str = scanner.nextLine();
        // String temp="";
             getReverseArray(str);
-            getReverseStream(str);
+           getReverseStream(str);
 
 
 
@@ -19,12 +19,16 @@ public class P43_ReverseString {
     public static void getReverseArray(String str)
     {
         char[] chArr = str.toCharArray();
+        StringBuilder sb = new StringBuilder();
 
         for(int i=str.length()-1; i>=0; i--)
         {
-            System.out.print(str.charAt(i));
+            //str+=str.charAt(i);
+            sb.append(str.charAt(i));
+          //  System.out.print(str.charAt(i));
         }
-        System.out.println();
+
+        System.out.println(sb);
     }
     public static void getReverseStream(String str)
     {
