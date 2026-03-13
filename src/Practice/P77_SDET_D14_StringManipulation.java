6d9b762e48fe4a12a385efaaa92f6e91dd0f8046
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-03-13 23:25:31 +0530

    "r@56vi Ku$%mar67"

---
+++ src/Practice/P77_SDET_D14_StringManipulation.java
@@ -0,0 +1,27 @@
+package Practice;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class P77_SDET_D14_StringManipulation {
+    public static void main() {
+        String str = "r@56vi Ku$%mar67";
+
+//        Pattern pattern = Pattern.compile("[a-zA-Z0-9]");
+//        Matcher matcher = pattern.matcher(str);
+        char[] carr = str.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        int sum=0;
+        for(char ch: carr)
+        {
+           if(Character.isLetter(ch)||ch==' ')
+            sb.append(ch);
+          else if(Character.isDigit(ch))
+              sum+=Character.getNumericValue(ch);
+
+        }
+
+        System.out.println(sb.toString()+""+sum);
+
+    }
+}
