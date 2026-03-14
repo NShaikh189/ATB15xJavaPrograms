159a48bbfab565e640db50d354adc7f07f8accfe
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-03-14 21:20:28 +0530

    two sum array

--- src/Practice/P71_TwoSumArray.java
+++ src/Practice/P71_TwoSumArray.java
@@ -1,14 +1,39 @@
 package Practice;
 
+import java.util.Arrays;
+
 import static Practice.P50_TwoSumHashSet.twoSum;
 
 public class P71_TwoSumArray {
 
-    void main()
-    {
-        int nums[] = {2,3,5,6,8,9,10,11};
+    void main() {
+        int[] nums = {0,1,2, 3, 5, 6,7, 8, 9, 10, 11};
         int target = 8;
 
-        twoSum(nums,target);
+       // twoSum(nums, target);
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int sum = nums[left]+nums[right];
+
+            if (sum == target) {
+                System.out.println(nums[left] + "," + nums[right]);
+                //break;
+                left++;
+                right--;
+                continue;
+            }
+
+            if(sum>target)
+            {
+                right--;
+            }
+            else if(sum<target)
+            {
+                left++;
+            }
+            }
+
     }
 }
