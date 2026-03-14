06c7b8c27b5ae40426fe4b42dc45b2b93adcdb38
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-03-14 21:02:54 +0530

    remove duplicates

--- src/Practice/P78_SDET_RemoveDuplicatesArray.java
+++ src/Practice/P78_SDET_RemoveDuplicatesArray.java
@@ -4,22 +4,27 @@ import java.util.*;
 
 public class P78_SDET_RemoveDuplicatesArray {
 
-    public static void main() {
-        int nums[] = {2,4,51,3,2,45,3};
+    static void main() {
+        int[] nums = {2, 4, 51, 3, 2, 45, 3};
         //[2,4,5,6,45]
 //List<Integer> nlist = new ArrayList<>();
 //for(int i : nums)
 //    nlist.add(i);
 
-Set<Integer> set = new LinkedHashSet<>();
+        Set<Integer> set = new LinkedHashSet<>();
 
-for(int i: nums)
-    set.add(i);
+        for (int i : nums)
+            set.add(i);
 
-List<Integer> nlist = new ArrayList<>(set);
-Collections.sort(nlist);
-        System.out.println(set.toString());
+        List<Integer> nlist = new ArrayList<>(set);
+        Collections.sort(nlist);
+        System.out.println(set);
         System.out.println(nlist);
 
+        System.out.println("----------------------");
+
+      List<Integer> ulist = Arrays.stream(nums).sorted().distinct().boxed().toList();
+        System.out.println(ulist);
     }
+
 }
